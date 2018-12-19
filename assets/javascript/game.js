396d84caf3d3030e581e92e1f390d7f600393a01
Alpa Patel <alpajp@live.com>
2018-12-18 22:03:59 -0500

    saving homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,71 @@
 var crystal1 = 0;
-var ctrstal2 = 0;
-var ctrstal3 = 0;
-var ctrstal4 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+var userTotal = 0;
+var wins = 0;
+var losses = 0;
+var num1;
+var num2;
+var num3;
+var num4;
+
+reset();
+
+function reset() {
+    
+    var random = Math.floor((Math.random() * 101) + 19);
+    $('#matchValue').text(random);
+    $('#totalScore').text(userTotal);
+    $('#wins').text(wins);
+    $('#losses').text(losses);
+
+    num1 = Math.floor(Math.random() * 11 + 1)
+    num2 = Math.floor(Math.random() * 11 + 1)
+    num3 = Math.floor(Math.random() * 11 + 1)
+    num4 = Math.floor(Math.random() * 11 + 1)
+}
+
+$("#crystal1").on("click", function () {
+    userTotal += num1;
+    $("#totalScore").text(userTotal);
+    if (totalScore === userTotal) {
+        wins++;
+        $('#wins').text(wins);
+        alert("You Won!!!");
+        reset();
+    }
+});
+$("#crystal2").on("click", function () {
+    userTotal += num1;
+    $("#totalScore").text(userTotal);
+    if (totalScore === userTotal) {
+        wins++;
+        $('#wins').text(wins);
+        alert("You Won!!!");
+        reset();
+    }
+});
+
+$("#crystal3").on("click", function () {
+    userTotal += num1;
+    $("#totalScore").text(userTotal);
+    if (totalScore === userTotal) {
+        wins++;
+        $('#wins').text(wins);
+        alert("You Won!!!");
+        reset();
+    }
+});
+
+$("#crystal4").on("click", function () {
+    userTotal += num1;
+    $("#totalScore").text(userTotal);
+    if (totalScore === userTotal) {
+        wins++;
+        $('#wins').text(wins);
+        alert("You Won!!!");
+        reset();
+    }
+});
 
